59288fd54a538b58480d093c54a2728c39648432
walshg3 <walshg3@go.stockton.edu>
2023-02-16 00:20:57 -0500

    first commit

---
+++ HW1/Main.java
@@ -0,0 +1,13 @@
+package HW1;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start of Main");
+        // Create an instance of the Pyramid class and call the makePyramid method (see Pyramid.java for more info)
+
+        // Create an instance of the Adlibs class and call the story method (see Adlibs.java for more info)
+
+        // Create an instance of the Replace class and call the charReplace method (see Replace.java for more info)
+
+    }
+}
---
+++ HW1/Pyramid.java
@@ -0,0 +1,21 @@
+package HW1;
+/*
+Make the output look like this:
+#
+##
+###
+####
+#####
+####
+###
+##
+#
+Hint: the object is created for you (Pyramid) and so is the method (Output)
+Hint2: Also notice the method is void meaning you will not be returning anything
+Hint3: The print statement is System.out.println();
+*/
+public class Pyramid {
+    public static void Output() {
+        
+    }
+}
