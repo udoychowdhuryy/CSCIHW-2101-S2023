e1c82348669cd5740a09f4dda359d2239b002f1a
walshg3 <walshg3@go.stockton.edu>
2023-04-11 00:34:55 -0400

    added Homework 3

---
+++ HW3/Loops.java
@@ -0,0 +1,49 @@
+package HW3;
+import java.util.Scanner;
+
+public class Loops {
+
+    //Given a string, return a new string where the first and last chars have been exchanged.
+
+
+    //frontBack("code") → "eodc"
+    //frontBack("a") → "a"
+    //frontBack("ab") → "ba"
+
+    public static String frontBack(String str){
+        return null; // <- this should be changed 
+    }
+
+    //Given 2 int values, return true if either of them is in the range 10..20 inclusive.
+    // in1020(12, 99) → true
+    // in1020(21, 12) → true
+    // in1020(8, 99) → false
+    public static boolean in1020(int a, int b) {
+        return false; // <- this should be changed 
+    } 
+
+    //Create a method Given a string, return the string where all of the "z"
+    //Have been removed. Except do not remove a z at the start
+    //or end
+    //Ex.
+    //stringT("zHelloz") -> "zHelloz"
+    //stringT("nozthaznks") -> "nothanks"
+    //stringT("xksiazdjaasldzsajzasdz") -> "xksiadjaasldsajasdz"
+    public static String stringZ(String str){
+        return null; // <- this should be changed 
+    }
+
+    //Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+    // makes10(9, 10) → true
+    // makes10(9, 9) → false
+    // makes10(1, 9) → true
+    public static boolean makes10(int a, int b) {
+
+        return false; // <- this should be changed 
+    }
+    public static void main(String[] args) {
+        // Add code to help test your methods here
+
+    }
+    
+}
